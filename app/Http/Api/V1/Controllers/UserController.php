4d382d7e276b39114da09fadccd01ef4095a5141
Danis Yogaswara <danistutorial@gmail.com>
2018-10-20 20:25:23 +0700

    add functional test

--- app/Http/Api/V1/Controllers/UserController.php
+++ app/Http/Api/V1/Controllers/UserController.php
@@ -16,7 +16,8 @@ class UserController extends Controller
      * @param \App\Services\Users\UserService user service
      * @return UserController
      */
-    public function __construct(UserService $service) {
+    public function __construct(UserService $service)
+    {
         $this->service = $service;
         $this->presenter = new JsonApiPresenter;
     }
@@ -25,7 +26,8 @@ class UserController extends Controller
      * Browse Users
      * @return \Illuminate\Http\JsonResponse
      */
-    public function browse(){
+    public function browse()
+    {
         $users = $this->service->browse();
         return $this->presenter->renderPaginator($users, 200);
     }
@@ -34,7 +36,8 @@ class UserController extends Controller
      * Read user by id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function read($id){
+    public function read($id)
+    {
         $user = $this->service->read($id);
         return $this->presenter->render($user, 200);
     }
@@ -44,44 +47,47 @@ class UserController extends Controller
      * @return \Illuminate\Http\JsonResßponse
      * @param \Illuminate\Http\Request $request
      */
-    public function edit($id, Request $request){
+    public function edit($id, Request $request)
+    {
         $user = $this->service->edit($id, $request);
         $headers = [
             'Content-Type' => 'application/vnd.api+json',
             'Accept' => 'application/vnd.api+json'
         ];
-        return $this->presenter->render($user, 200, $headers);
+        return $this->presenter->render($user, 201, $headers);
     }
 
     /**
      * Add user
      * @param \Illuminate\Http\Request $request
      */
-    public function add(Request $request){
+    public function add(Request $request)
+    {
         $user = $this->service->add($request);
         $headers = [
             'Content-Type' => 'application/vnd.api+json',
             'Accept' => 'application/vnd.api+json'
         ];
-        return $this->presenter->render($user, 200, $headers);
+        return $this->presenter->render($user, 201, $headers);
     }
 
     /**
      * Delete User by id
      */
-    public function delete($id){
+    public function delete($id)
+    {
         $deleted = $this->service->delete($id);
 
         return response()->json([
             'meta' => [
                 'deleted_count' => $deleted,
             ]
-        ], 200);
+        ], 204);
     }
 
     public function register(Request $request)
     {
-         $data = [
+        $data = [
              'first_name' => $request->first_name,
              'last_name' => $request->last_name,
              'email' => $request->email,
@@ -89,12 +95,11 @@ class UserController extends Controller
              'phone' => $request->phone,
              'country' => $request->country
          ];
-         $user = $this->service->register($data);
-         $headers = [
+        $user = $this->service->register($data);
+        $headers = [
             'Content-Type' => 'application/vnd.api+json',
             'Accept' => 'application/vnd.api+json'
          ];
-         return $this->presenter->render($user, 200, $headers);
+        return $this->presenter->render($user, 200, $headers);
     }
-
 }
